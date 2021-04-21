3aad4be48b5c5e51833173aaa3b5d531e6878940
imamfahmi29 <imam.fahmi29@gmail.com>
2021-04-21 11:34:43 +0700

    perbaiki halaman unggah dokumen

--- resources/views/frontend/unggah.blade.php
+++ resources/views/frontend/unggah.blade.php
@@ -39,13 +39,16 @@
 @endsection
 @section('content')
 <div class="container py-3">
-    <h2>Pengajuan Surat Keterangan Usaha</h2>
+    <center>
+    <h3>Halaman Unggah Dokumen</h3>
+    </center>
+    
     <div class="line"></div>
     <!-- content -->
     <form action="{{route('frontend.unggah.proses')}}" method="post" enctype="multipart/form-data" id="mainform">
         {{csrf_field()}}
         <div class="form-group row">
-            <label for="" class="col-sm-3 col-form-label">Scan/Foto KTP</label>
+            <label for="" class="col-sm-4 col-form-label">Scan/Foto KTP</label>
             <div class="col-sm-8 pl-2">
                 <div class="custom-file">
                 <input type="file" class="custom-file-input" name="file_ktp" id="file_ktp">
@@ -60,7 +63,7 @@
             </div>
         </div>
         <div class="form-group row">
-            <label for="" class="col-sm-3 col-form-label">Scan/Foto Kartu Keluarga</label>
+            <label for="" class="col-sm-4 col-form-label">Scan/Foto Kartu Keluarga</label>
             <div class="col-sm-8 pl-2">
                 <div class="custom-file">
                     <input type="file" class="custom-file-input" name="file_kk" id="file_kk">
@@ -74,14 +77,14 @@
                 <small class="w-100"> *) file type: jpg/jpeg/png | max size: 1 MB</small>
             </div>
         </div>
+        <div class="line"></div>
         <div class="row">
-            <div class="col-lg-11">
+            <div class="col-lg-12">
                 <button type="submit" id="tes" class="btn btn-gelap float-right">{{($unggah)?'Ubah':'Simpan'}}</button>
             </div>
         </div>
     </form>
     <br />
-    <div class="line"></div>
 </div>
 
 @endsection
